cc40ea29e1a308838bdb274446075b15bda25f30
krasnowsky <wojtek.ref2@gmail.com>
2022-06-21 13:36:39 +0200

    Article model added

--- src/articles/models.py
+++ src/articles/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.TextField()
-    date = models.TextField()
+    author = models.CharField(max_length=40)
+    #auto_now = True
+    date = models.DateTimeField()
