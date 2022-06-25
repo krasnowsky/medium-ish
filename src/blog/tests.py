d718f20e113e011965bbed3fce53b245b43e73bb
krasnowsky <wojtek.ref2@gmail.com>
2022-06-25 13:34:21 +0200

    Added ViewsTests

--- src/blog/tests.py
+++ src/blog/tests.py
@@ -1,7 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 from .models import Author, Note, Article, Tag
 
 # Create your tests here.
+
 class URLTests(TestCase):
     def setUp(self):
         test_note = Note.objects.create(content='TEST CONTENT')
@@ -67,3 +69,38 @@ class ModelsTests(TestCase):
         self.assertEqual(article.title, 'test_title')
         self.assertEqual(article.content, 'test_content')
         self.assertEqual(article.author, author)
+
+class ViewTests(TestCase):
+    def setUp(self):
+        client = Client()
+        test_note = Note.objects.create(content='TEST CONTENT')
+        test_note.save()
+
+        test_author = Author.objects.create(name='test_name', surname='test_surname', note=test_note)
+        test_author.save()
+
+        test_tag = Tag.objects.create(name='test_tag')
+        test_tag.save()
+
+        test_article = Article.objects.create(title='test_title', content='test_content', author=test_author)
+        test_article.save()
+
+    def test_client_home_page_responding(self):
+        response = self.client.get(reverse('home_page'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_client_tag_page_responding(self):
+        response = self.client.get(reverse('tag_page'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_client_article_page_responding(self):
+        article = Article.objects.get(title='test_title')
+        slug = article.slug
+        response = self.client.get(reverse('detail', args=[slug]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_client_author_page_responding(self):
+        author = Author.objects.get(name='test_name')
+        slug = author.slug
+        response = self.client.get(reverse('detail_author', args=[slug]))
+        self.assertEqual(response.status_code, 200)
